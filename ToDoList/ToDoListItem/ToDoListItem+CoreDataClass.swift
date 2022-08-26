1fff6e0b55312f4df5f76fe76ffac6dfb1d5b97c
Vasu Gupta <84150496+vasu25@users.noreply.github.com>
2022-08-26 21:33:21 +0530

    code files

---
+++ ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Vasu Gupta on 26/08/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
